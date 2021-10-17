0225a2e614ba1649054005157fe2a3225ab6c3cc
Ben Hur Faria <benhurfaria3@gmail.com>
2021-10-17 15:16:04 -0300

    games post

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ app.post("/categories", async (req, res) =>{
         res.status(409).send("Esta categoria já existe");
     }else{
         const result = await connection.query(`INSERT INTO categories (name) VALUES ($1);`, [name]);
-    res.send("deu bom");
+        res.send("deu bom");
     }    
 });
 
@@ -43,15 +43,19 @@ app.post("/categories", async (req, res) =>{
 app.get("/games", async (req, res) =>{
     const {name} = req.query;
     
+    try{
+        if(name !== undefined){
+            const entrada = name + "%";
+            const resultParte = await connection.query(`SELECT * FROM games WHERE name LIKE $1;`,[entrada]);
+            res.send(resultParte.rows);
 
-    if(name !== undefined){
-        const entrada = name + "%";
-        const resultParte = await connection.query(`SELECT * FROM games WHERE name LIKE $1;`,[entrada]);
-        res.send(resultParte.rows);
-    }
-    else{
-        const result = await connection.query(`SELECT * FROM games;`);
-        res.send(result.rows);
+        }
+        else{
+            const result = await connection.query(`SELECT * FROM games;`);
+            res.send(result.rows);
+        }
+    }catch{
+        res.sendStatus(500);
     }
 });
 
@@ -68,12 +72,26 @@ app.post("/games", async (req,res)=>{
         res.status(400).send("Nome vazio");
     }
 
-    if(stockTotal === 0 || pricePerDay === 0){
+    if(stockTotal <= 0 || pricePerDay <= 0){
         res.status(400).send("Valor inválido");
     }
+
+    try{
+        console.log("OI");
+        const resultNome = await connection.query(`SELECT * FROM games WHERE name = $1`, [name]);
+        if(resultNome.rows.length !==0){
+            res.sendStatus(409);
+        }else{
+            const result = await connection.query(`INSERT INTO games (name, image, "stockTotal", "categoryId", "pricePerDay") VALUES ($1, $2, $3, $4, $5);`, [name,image, stockTotal, categoryId, pricePerDay]);
+            res.send("deu bom");
+        }
+    }catch{
+        res.sendStatus(500);
+    }
     
-    
-})
+});
+
+
 
 
 
