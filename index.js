b36821640fc32c31204a74e4c04a81ef7ffca7be
Ben Hur Faria <benhurfaria3@gmail.com>
2021-10-16 23:47:31 -0300

    games

--- index.js
+++ index.js
@@ -17,24 +17,63 @@ const connection = new Pool({
 });
 
 
-app.get("/categories", (req, res) =>{
-    const promise = connection.query(`SELECT * FROM categories;`);
-    try{
-    promise.then(result =>{
-        console.log(result);
-        res.send(result.rows);
-    })
-    }catch(err){
-        res.send(err);
-    }
-   
+app.get("/categories", async (req, res) =>{
+    const result = await connection.query(`SELECT * FROM categories;`);
+    res.send(result.rows);
 });
 
-app.post("/categories", (req, res) ={
-   
-})
+app.post("/categories", async (req, res) =>{
+    const {name} = req.body;
+    
+    if(name.length === 0) {
+        res.status(400).send("Não pode estar vazio");
+    }
 
- //res.send(categorias);
+    const resultConsulta = await connection.query(`SELECT * FROM categories WHERE name = $1`, [name]);
+
+    if(resultConsulta.rows.length !== 0) {
+        res.status(409).send("Esta categoria já existe");
+    }else{
+        const result = await connection.query(`INSERT INTO categories (name) VALUES ($1);`, [name]);
+    res.send("deu bom");
+    }    
+});
+
+
+app.get("/games", async (req, res) =>{
+    const {name} = req.query;
+    
+
+    if(name !== undefined){
+        const entrada = name + "%";
+        const resultParte = await connection.query(`SELECT * FROM games WHERE name LIKE $1;`,[entrada]);
+        res.send(resultParte.rows);
+    }
+    else{
+        const result = await connection.query(`SELECT * FROM games;`);
+        res.send(result.rows);
+    }
+});
+
+app.post("/games", async (req,res)=>{
+    const {
+        name,
+        image,
+        stockTotal,
+        categoryId,
+        pricePerDay
+    } = req.body;
+
+    if(name.length === 0){
+        res.status(400).send("Nome vazio");
+    }
+
+    if(stockTotal === 0 || pricePerDay === 0){
+        res.status(400).send("Valor inválido");
+    }
+    
+    
+})
 
 
 
